1499a8d23e1863b4174bd3a95595f4417878013f
Arwyne De Guzman <arwynedeguzman@gmail.com>
2022-03-19 23:50:15 +0800

    setup database connection

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,25 @@
-const express = require("express")
-const dotenv = require("dotenv")
-const morgan = require("morgan")
-require("colors")
+const express = require("express");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+require("colors");
+const connectDB = require("./config/db");
 
-dotenv.config()
+dotenv.config();
+connectDB();
 
-const app = express()
-const PORT = process.env.PORT || 5000
+const app = express();
+const PORT = process.env.PORT || 5000;
 
 if (process.env.NODE_ENV === "dev") {
-  app.use(morgan("dev"))
+  app.use(morgan("dev"));
 }
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
 
 app.listen(PORT, () => {
   console.log(
     `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
-  )
-})
+  );
+});
